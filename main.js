ce4b74455aad104bb5a00d6f5dbb377b0259230f
Daniel Isham <danielisham1@gmail.com>
2017-05-15 17:02:14 -0500

    navbar and pages outlined

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ function fetchData() {
 
     let placesKey = 'AIzaSyCpTvE8KrIFGMdXXPHmIDzMKIzZ3xav9JQ';
     let mapsKey = 'AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w';
+    let corsProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
+    let denver = '39.747704,-104.990607'
 
-    let request = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyCpTvE8KrIFGMdXXPHmIDzMKIzZ3xav9JQ';
+    let request = corsProxy +  'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + denver + '&radius=500&type=restaurant&keyword=brewery&key=' + placesKey;
 
     let $xhr = $.getJSON(request);
 
@@ -16,7 +18,13 @@ function fetchData() {
       if ($xhr.status !== 200) {
         return;
       }
-      console.log(data);
+      console.log(data.results);
+
+      for (let i = 0; i < data.results.length; i++) {
+        let $results_pane = $('#results-pane');
+        let name = data.results[i]['name'];
+        $results_pane.append(`<p>${name}</p>`);
+      }
 
     });
 

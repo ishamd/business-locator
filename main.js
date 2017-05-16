723d09099b4e120951cd3cafb8f914620279202d
Daniel Isham <danielisham1@gmail.com>
2017-05-16 11:16:53 -0500

    navbar functionality added

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
+let placesKey = 'AIzaSyCpTvE8KrIFGMdXXPHmIDzMKIzZ3xav9JQ';
+let mapsKey = 'AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w';
+let corsProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
 let results = [];
-let cityName = '';
+let cityName;
 let cityPosition = {};
+let coords = '39.747704,-104.990607';
+let radius = 500;
+let type = 'establishment';
+let keyword = 'brewery';
+let name = 'brew';
+
+// Store location input to global variable
+let $button = $('button');
+let $location_input = $('#location-input');
+$button.click(function(event) {
+  event.preventDefault();
+  cityName = $location_input.val();
+})
+
+// change dropdown-menu to display selection
+$(".dropdown-menu li a").click(function() {
+  $(this).parents(".dropdown").find('#choice').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('#choice').val($(this).data('value'));
+
+  if ($(this).text() === 'Breweries') {
+    keyword = 'brewery';
+    name = 'brew';
+    fetchData();
+  };
+
+  if ($(this).text() === 'Wineries') {
+    keyword = 'winery';
+    name = 'wine';
+    fetchData();
+  };
+
+  if ($(this).text() === 'Distilleries') {
+    keyword = 'distillery';
+    name = 'whiskey';
+    fetchData();
+  };
+
+});
+
 
 function fetchData() {
 
   $(document).ready(function() {
     console.log('Doc is ready');
 
-    let placesKey = 'AIzaSyCpTvE8KrIFGMdXXPHmIDzMKIzZ3xav9JQ';
-    let mapsKey = 'AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w';
-    let corsProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
-    let location = '39.747704,-104.990607'
-    let radius = 500;
-    let type = 'establishment';
-    let keyword = 'brewery';
-    let name = 'brew';
-
-    let request = `${corsProxy}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&name=${name}&radius=${radius}&type=${type}&keyword=${keyword}&key=${placesKey}`
+    // make API request to google places
+    let request = `${corsProxy}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&name=${name}&radius=${radius}&type=${type}&keyword=${keyword}&key=${placesKey}`
+    console.log(request);
 
     let places_$xhr = $.getJSON(request);
 
@@ -25,11 +60,14 @@ function fetchData() {
         return;
       }
 
+      results = [];
+      let $results_pane = $('#results-pane');
+      $results_pane.empty();
+
       for (let i = 0; i < data.results.length; i++) {
-        let $results_pane = $('#results-pane');
+        // let $results_pane = $('#results-pane');
         let name = data.results[i]['name'];
         let position = data.results[i]['geometry']['location'];
-        // console.log(position);
         let result = {
           'name': name,
           'position': {
@@ -38,15 +76,12 @@ function fetchData() {
           }
         }
         results.push(result)
-        // console.log(result);
         $results_pane.append(`<p>${name}</p>`);
-
       }
 
+      // append empty google map to page
       let $body = $('body');
-
       let script = '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w&callback=initMap"></script>'
-
       $body.append(script);
 
     });
@@ -59,6 +94,7 @@ function fetchData() {
 
 }
 
+// initialize the map and drop markers on the page
 function initMap() {
 
   var uluru = {
@@ -66,7 +102,7 @@ function initMap() {
     lng: -104.990607
   };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
+    zoom: 12,
     center: uluru
   });
   // // original marker set from google docs:
@@ -76,17 +112,14 @@ function initMap() {
   // });
 
   for (let j = 0; j < results.length; j++) {
-
     let marker = new google.maps.Marker({
       position: new google.maps.LatLng({
         lat: results[j].position.lat,
         lng: results[j].position.lng
       }),
     });
-    // console.log(results)
     marker.setMap(map);
   }
-  console.log(results)
 }
 
 // This is an Init function which will fire when the page loads.

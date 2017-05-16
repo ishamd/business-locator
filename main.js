08b887aeb18fe26cb3fce49c0fe38d8fd9c409d8
Daniel Isham <danielisham1@gmail.com>
2017-05-16 12:06:56 -0500

    geocoding search added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w'
+
 let placesKey = 'AIzaSyCpTvE8KrIFGMdXXPHmIDzMKIzZ3xav9JQ';
 let mapsKey = 'AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w';
 let corsProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
 let results = [];
-let cityName;
+let address = 'Denver';
+let addressLat;
+let addressLng;
 let cityPosition = {};
-let coords = '39.747704,-104.990607';
+let coords = `${addressLat},${addressLng}`;
 let radius = 500;
 let type = 'establishment';
 let keyword = 'brewery';
@@ -15,7 +19,8 @@ let $button = $('button');
 let $location_input = $('#location-input');
 $button.click(function(event) {
   event.preventDefault();
-  cityName = $location_input.val();
+  address = $location_input.val();
+  fetchData();
 })
 
 // change dropdown-menu to display selection
@@ -49,61 +54,84 @@ function fetchData() {
   $(document).ready(function() {
     console.log('Doc is ready');
 
-    // make API request to google places
-    let request = `${corsProxy}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&name=${name}&radius=${radius}&type=${type}&keyword=${keyword}&key=${placesKey}`
-    console.log(request);
+    // make geocodeAPI request
+    let geoRequest = `${corsProxy}https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w`
 
-    let places_$xhr = $.getJSON(request);
+    let geo_$xhr = $.getJSON(geoRequest);
 
-    places_$xhr.done(function(data) {
-      if (places_$xhr.status !== 200) {
+    geo_$xhr.done(function(obj) {
+      if (geo_$xhr.status !== 200) {
         return;
       }
 
-      results = [];
-      let $results_pane = $('#results-pane');
-      $results_pane.empty();
+      console.log(obj.results[0].geometry.location);
+      addressLat = obj.results[0].geometry.location.lat;
+      console.log(addressLat);
+      addressLng = obj.results[0].geometry.location.lng;
+      console.log(addressLng);
+      coords = `${addressLat},${addressLng}`;
 
-      for (let i = 0; i < data.results.length; i++) {
-        // let $results_pane = $('#results-pane');
-        let name = data.results[i]['name'];
-        let position = data.results[i]['geometry']['location'];
-        let result = {
-          'name': name,
-          'position': {
-            lat: position.lat,
-            lng: position.lng
-          }
+      // make API request to google places
+      let nearbyRequest = `${corsProxy}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&name=${name}&radius=${radius}&type=${type}&keyword=${keyword}&key=${placesKey}`
+      console.log(nearbyRequest);
+
+      let places_$xhr = $.getJSON(nearbyRequest);
+
+      places_$xhr.done(function(data) {
+        if (places_$xhr.status !== 200) {
+          return;
         }
-        results.push(result)
-        $results_pane.append(`<p>${name}</p>`);
-      }
 
-      // append empty google map to page
-      let $body = $('body');
-      let script = '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w&callback=initMap"></script>'
-      $body.append(script);
+        results = [];
+        let $results_pane = $('#results-pane');
+        $results_pane.empty();
+
+        for (let i = 0; i < data.results.length; i++) {
+          // let $results_pane = $('#results-pane');
+          let name = data.results[i]['name'];
+          let position = data.results[i]['geometry']['location'];
+          let result = {
+            'name': name,
+            'position': {
+              lat: position.lat,
+              lng: position.lng
+            }
+          }
+          results.push(result)
+          $results_pane.append(`<p>${name}</p>`);
+        }
+
+        // append empty google map to page
+        let $body = $('body');
+        let script = '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoThcP6tdQR8VR3xcjnZbjzTomgZD3B2w&callback=initMap"></script>'
+        $body.append(script);
+
+      });
+
+      places_$xhr.fail(function(err) {
+        console.log(err);
+      });
 
     });
 
-    places_$xhr.fail(function(err) {
+    geo_$xhr.fail(function(err) {
       console.log(err);
     });
 
   });
 
-}
+};
 
 // initialize the map and drop markers on the page
 function initMap() {
 
-  var uluru = {
-    lat: 39.747704,
-    lng: -104.990607
+  var center = {
+    lat: addressLat,
+    lng: addressLng
   };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: uluru
+    center: center
   });
   // // original marker set from google docs:
   // var marker = new google.maps.Marker({
